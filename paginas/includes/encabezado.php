8f399f3f53a0c38c5dc95c57997b6b0ca02a50d5
aldajassofl <aldajassofl@ittepic.edu.mx>
2023-09-14 07:59:22 -0700

    Encabezado de marin

--- paginas/includes/encabezado.php
+++ paginas/includes/encabezado.php
@@ -11,6 +11,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mercado Libre</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&family=Pacifico&family=Patua+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/formulario.css">
